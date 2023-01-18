70cec84a884cc9d435288ff6c968d242fe3c5584
SUHAIB Afzal <sxa1556@cs.bham.ac.uk>
2023-01-18 20:46:16 +0000

    Example Code Added

--- src-exe/Main.hs
+++ src-exe/Main.hs
@@ -1,4 +1,36 @@
-module Main where
+{-# LANGUAGE OverloadedStrings #-}  -- allows "string literals" to be Text
+import           Control.Monad (when, void)
+import           UnliftIO.Concurrent
+import           Data.Text (isPrefixOf, toLower, Text)
+import qualified Data.Text.IO as TIO
 
-main :: IO ()
-main = putStrLn "Hello, Haskell!"
+import           Discord
+import           Discord.Types
+import qualified Discord.Requests as R
+
+-- | Replies "pong" to every message that starts with "ping"
+pingpongExample :: IO ()
+pingpongExample = do
+    userFacingError <- runDiscord $ def
+             { discordToken = "Bot ZZZZZZZZZZZZZZZZZZZ"
+             , discordOnEvent = eventHandler
+             , discordOnLog = \s -> TIO.putStrLn s >> TIO.putStrLn ""
+             } -- if you see OnLog error, post in the discord / open an issue
+
+    TIO.putStrLn userFacingError
+    -- userFacingError is an unrecoverable error
+    -- put normal 'cleanup' code in discordOnEnd (see examples)
+
+eventHandler :: Event -> DiscordHandler ()
+eventHandler event = case event of
+    MessageCreate m -> when (isPing m && not (fromBot m)) $ do
+        void $ restCall (R.CreateReaction (messageChannelId m, messageId m) "eyes")
+        threadDelay (2 * 10^6)
+        void $ restCall (R.CreateMessage (messageChannelId m) "Pong!")
+    _ -> return ()
+
+fromBot :: Message -> Bool
+fromBot = userIsBot . messageAuthor
+
+isPing :: Message -> Bool
+isPing = ("ping" `isPrefixOf`) . toLower . messageContent
